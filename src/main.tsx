7c75e9ea9d80625f2bcfaf0b665aa9da52ac6b3e
Alaa Shalaby <alaashalabi011@gmail.com>
2024-10-19 01:15:05 +0200

    Wrapp App inside CartContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import CartContextProvider from "./context/CartContextProvider.tsx";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <ChakraProvider>
-      <App />
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
     </ChakraProvider>
   </QueryClientProvider>
 );
