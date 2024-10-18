9b5221f3425b2a9122f000e0a8ed507461a4eb42
Alaa Shalaby <alaashalabi011@gmail.com>
2024-10-18 23:10:02 +0200

    Add interface types to data

---
+++ src/types.d.ts
@@ -0,0 +1,22 @@
+interface ProductsResponse{
+    data: Product[];
+}
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  images: string[];
+  creationAt: string;
+  updatedAt: string;
+  category: Category;
+}
+
+interface Category {
+  id: number;
+  name: string;
+  image: string;
+  creationAt: string;
+  updatedAt: string;
+}
