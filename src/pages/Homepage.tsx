9f34ea6d4c7c98ac12f9b33c404852c80457431f
Alaa Shalaby <alaashalabi011@gmail.com>
2024-10-19 02:09:52 +0200

    Add Components to HomePage

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,15 @@
+import AboutUs from "../components/AboutUs";
+import Header from "../components/Header";
+import Products from "../components/Products";
+
+const Homepage = () => {
+  return (
+    <>
+      <Header />
+      <AboutUs />
+      <Products />
+    </>
+  );
+}
+
+export default Homepage
