7807d7c8245ac31893f261e1aeef87775a68c4a8
Alaa Shalaby <alaashalabi011@gmail.com>
2024-10-19 12:52:18 +0200

    Add route for ProductsPage

--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import Layout from "./components/Layout";
 import CartPage from "./pages/CartPage";
 import Homepage from "./pages/Homepage";
+import ProductsPage from "./pages/ProductsPage";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Layout />}>
             <Route index element={<Homepage />} />
+            <Route path="products" element={<ProductsPage />} />
             <Route path="cart" element={<CartPage/>}/>
         </Route>
     )
